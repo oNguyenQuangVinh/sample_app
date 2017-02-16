e9547da3b981788353bfb84468681f8ae691180d
vinhnguyen2210 <iicanfly2210@gmail.com>
2017-02-16 13:21:21 +0700

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-   render html: "USER HERE"
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
